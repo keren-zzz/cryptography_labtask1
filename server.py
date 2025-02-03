1d683abec7d4000900174a599133dec6a4cd0492
keren-zzz <kerenmary.edu@gmail.com>
2025-02-03 15:58:11 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 import socket
 
+# Caesar Cipher Decryption Function
+def caesar_decrypt(text, shift):
+    decrypted = ""
+    for char in text:
+        if char.isalpha():
+            shift_value = 65 if char.isupper() else 97
+            decrypted += chr((ord(char) - shift_value - shift) % 26 + shift_value)
+        else:
+            decrypted += char
+    return decrypted
+
 def server_program():
-    # Get the host and port
     host = "127.0.0.1"  # Localhost
     port = 12345  # Port number
 
@@ -14,18 +24,25 @@ def server_program():
     conn, address = server_socket.accept()  # Accept connection
     print(f"Connection from {address}")
 
+    shift = 3  # Caesar cipher shift (same shift used by the client)
+    
     while True:
         # Receive data from client
         data = conn.recv(1024).decode()
         if not data:
             break  # If no data, close connection
 
-        print("Received message:", data)
-        response = "Server response: " + data
+        print("Encrypted message received:", data)
+
+        # Decrypt message using Caesar cipher
+        decrypted_message = caesar_decrypt(data, shift)
+        print("Decrypted message:", decrypted_message)
+
+        # Send back the decrypted message as server response
+        response = "Server response: " + decrypted_message
         conn.send(response.encode())  # Send response back to client
 
     conn.close()  # Close connection
 
 if __name__ == '__main__':
     server_program()
-
