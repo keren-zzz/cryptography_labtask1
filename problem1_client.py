388ddc9de91875ec1c86157e748e72bfdcc387f9
keren-zzz <kerenmary.edu@gmail.com>
2025-02-16 00:53:36 +0530

    Update problem1_client.py

--- problem1_client.py
+++ problem1_client.py
@@ -1,24 +1,24 @@
 import socket
 
 def client_program():
-    host = "127.0.0.1"  # Server address
-    port = 12345  # Port number
+    host = "127.0.0.1" 
+    port = 12345  
 
-    client_socket = socket.socket()  # Create socket object
-    client_socket.connect((host, port))  # Connect to server
+    client_socket = socket.socket()  
+    client_socket.connect((host, port)) 
 
     while True:
-        message = input("Enter message to send to server: ")  # Get user input
+        message = input("Enter message to send to server: ")  
 
-        client_socket.send(message.encode())  # Send message to server
-        data = client_socket.recv(1024).decode()  # Receive response from server
+        client_socket.send(message.encode()) 
+        data = client_socket.recv(1024).decode()  
 
         print("Server:", data)
 
         if message.lower() == "exit":
-            break  # Exit the loop if 'exit' message is sent
+            break  
 
-    client_socket.close()  # Close the connection
+    client_socket.close() 
 
 if __name__ == '__main__':
     client_program()
