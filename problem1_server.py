fb56099bdf3f36ebcd66f33bf02e34d54b198367
keren-zzz <kerenmary.edu@gmail.com>
2025-02-16 00:54:24 +0530

    Update problem1_server.py

--- problem1_server.py
+++ problem1_server.py
@@ -1,30 +1,30 @@
 import socket
 
 def server_program():
-    # Get the host and port
-    host = "127.0.0.1"  # Localhost
-    port = 12345  # Port number
+ 
+    host = "127.0.0.1"  
+    port = 12345  
 
-    server_socket = socket.socket()  # Create socket object
-    server_socket.bind((host, port))  # Bind the host and port
+    server_socket = socket.socket() 
+    server_socket.bind((host, port))  
 
-    server_socket.listen(1)  # Allow one connection at a time
+    server_socket.listen(1) 
     print(f"Server is listening on {host}:{port}...")
     
-    conn, address = server_socket.accept()  # Accept connection
+    conn, address = server_socket.accept() 
     print(f"Connection from {address}")
 
     while True:
-        # Receive data from client
+      
         data = conn.recv(1024).decode()
         if not data:
-            break  # If no data, close connection
+            break 
 
         print("Received message:", data)
         response = "Server response: " + data
-        conn.send(response.encode())  # Send response back to client
+        conn.send(response.encode())  
 
-    conn.close()  # Close connection
+    conn.close()  
 
 if __name__ == '__main__':
     server_program()
